d55253ad1412d09647398d2ca6cecd7aa8705e01
Ch0min <marklundgaard7@gmail.com>
2023-08-29 18:08:46 +0200

    Added dummy assets and styling to App component to begin with.

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {StyleSheet, View, Text, Image, ImageBackground} from "react-native";
 
-export default function App() {
+const App = () => {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <View style={styles.pageContainer}>
+        <View style={styles.card}>
+          <ImageBackground
+              source={{uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg"}}
+
+              style={styles.image}
+          />
+
+        </View>
+      </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  pageContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1
   },
-});
+  card: {
+    width: "95%",
+    height: "70%",
+    borderRadius: 10,
+
+    shadowColor: "#000",
+    shadowOffset: { // for ios
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+    elevation: 11
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
+    overflow: "hidden"
+  }
+})
+
+export default App
