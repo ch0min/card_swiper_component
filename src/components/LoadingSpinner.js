b54c169469e1fc66850cb3bb352091e29ec1aa20
Ch0min <marklundgaard7@gmail.com>
2023-09-08 18:32:21 +0200

    Done: - Refactoring

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -7,7 +7,7 @@ const LoadingSpinner = ({children}) => {
     useEffect(() => {
         const timer = setTimeout(() => {
             setIsLoading(false)
-        }, 1000)
+        }, 500)
         return () => clearTimeout(timer)
     }, [])
 
